d39ebe63da1420897ce616cd31cdbe77dc064277
Myk251 <168402712+Myk251@users.noreply.github.com>
2024-05-12 17:21:05 +0300

    Add files via upload

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, send_from_directory, abort
-from flask_cors import CORS
 import os
 
-# Імпорт необхідних файлів для Students
-from Models.students_model import initialize_students_db
 from API.students_api import students_api as students_api
 from API.students_route import students_route
+from API.technique_api import technique_api
+from API.technique_route import technique_route
+# Імпорт необхідних файлів для Students
+from Models.students_model import initialize_students_db
+
+# Імпорт необхідного для Technique
+from Models.technique_model import initialize_technique_db
+
+from flask import Flask, send_from_directory, abort
+from flask_cors import CORS
 
 # Ініціалізація app та шляху до статичних даних
 app = Flask(__name__, static_folder='../Frontend')
@@ -16,9 +22,16 @@ CORS(app)
 # Ініціалізація бази даних
 initialize_students_db()
 
+# Ініціалізація бази даних для Technicue
+initialize_technique_db()
+
 # Реєстрація Blueprints для API та Навігції FrontEnd в браузері
 app.register_blueprint(students_api, url_prefix='/api')
 app.register_blueprint(students_route)
+
+# Реєстрація Blueprints для API та Навігції FrontEnd в браузері
+app.register_blueprint(technique_api, url_prefix='/api')
+app.register_blueprint(technique_route)
 
 
 # Точка входу, index.html
@@ -35,15 +48,27 @@ def send_js(path):
         # Забороняємо доступ до шляху, якщо він містить небезпечні символи
         abort(404)
 
-    # Формуємо абсолютний шлях до файлу для безпечного доступу
-    safe_path = os.path.abspath(os.path.join(students_route.static_folder, path))
+    if path.startswith('/students'):
+        # Формуємо абсолютний шлях до файлу для безпечного доступу
+        safe_path = os.path.abspath(os.path.join(students_route.static_folder, path))
 
-    # Перевіряємо, чи знаходиться файл дійсно у дозволеній директорії
-    if not safe_path.startswith(os.path.abspath(students_route.static_folder)):
-        abort(404)
+        # Перевіряємо, чи знаходиться файл дійсно у дозволеній директорії
+        if not safe_path.startswith(os.path.abspath(students_route.static_folder)):
+            abort(404)
 
-    # Відправляємо файл за безпечним шляхом
-    return send_from_directory(students_route.static_folder, path)
+        # Відправляємо файл за безпечним шляхом
+        return send_from_directory(students_route.static_folder, path)
+
+    if path.startswith('/technique'):
+        # Формуємо абсолютний шлях до файлу для безпечного доступу
+        safe_path = os.path.abspath(os.path.join(technique_route.static_folder, path))
+
+        # Перевіряємо, чи знаходиться файл дійсно у дозволеній директорії
+        if not safe_path.startswith(os.path.abspath(technique_route.static_folder)):
+            abort(404)
+
+        # Відправляємо файл за безпечним шляхом
+        return send_from_directory(technique_route.static_folder, path)
 
 
 # Запуск сервера в режимі тестування
